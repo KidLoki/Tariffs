df9e0875aa2f5ddf3a69fbc66264b715cc706ca2
Rhiana Heath <rhiana@heath.cc>
2014-02-18 20:36:49 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tariffs::Application.config.secret_key_base = '51bf8aec071643c10e7e7f3b90480557f708347dc76f48af2333b625e0990a7174b973dee222266c3dfe0910ac8d7189ae41f5f7d4ba40a4e467af7427732b12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tariffs::Application.config.session_store :cookie_store, key: '_tariffs_session'
